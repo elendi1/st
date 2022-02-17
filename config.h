92b30951e0b148c67b4263e5aed9321f8b5c4e8e
elendi1 <2ti7xaoj@anonaddy.me>
2022-02-17 17:42:03 +0100

    Added shortcuts to edit scrollback buffer and to open url

--- config.h
+++ config.h
@@ -243,8 +243,23 @@ static MouseShortcut mshortcuts[] = {
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
 
+static char *openurlcmd[] = { 
+  "/bin/sh",
+  "-c",
+  "linkgrabber",
+  "externalpipe",
+  NULL
+};
+
+static char *openeditorcmd[] = { 
+  "/bin/sh",
+  "-c",
+  "editscreen",
+  "externalpipe",
+  NULL
+};
 /* Internal keyboard shortcuts. */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
 static Shortcut shortcuts[] = {
@@ -263,6 +278,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ TERMMOD,              XK_E,           externalpipe,   {.v =  openeditorcmd} },
+	{ TERMMOD,              XK_U,           externalpipe,   {.v =  openurlcmd} },
 };
 
 /*
